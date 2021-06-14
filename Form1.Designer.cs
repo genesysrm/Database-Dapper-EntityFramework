2011f950e48ba039634658ee72c045f40936dc08
Genesys Rondon <servicosgesoft@gmail.com>
2021-06-14 17:58:43 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace CapaConectada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btncargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(743, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btncargar
+            // 
+            this.btncargar.Location = new System.Drawing.Point(34, 192);
+            this.btncargar.Name = "btncargar";
+            this.btncargar.Size = new System.Drawing.Size(118, 23);
+            this.btncargar.TabIndex = 1;
+            this.btncargar.Text = "Cargar información";
+            this.btncargar.UseVisualStyleBackColor = true;
+            this.btncargar.Click += new System.EventHandler(this.btncargar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btncargar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btncargar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CapaConectada.Models;
+
+namespace CapaConectada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncargar_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection conexion = new SqlConnection(@"Data Source=LAPTOP-Q6ODQIT5\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            conexion.Open();
+
+            string sql = "SELECT * FROM [dbo].[Customers]";
+
+            SqlCommand comando = new SqlCommand(sql, conexion);
+            SqlDataReader datareader = comando.ExecuteReader();
+
+            List<Customer> customer = new List<Customer>();
+
+            while (datareader.Read())
+            {
+                Customer customers = new Customer();
+                customers.CompanyName = datareader.GetString(0);
+                customers.ContactName = datareader.GetString(1);
+                customers.ContactTitle = datareader.GetString(2);
+                customers.Address = datareader.GetString(3);
+                customers.City = datareader.GetString(4);
+                customers.Region = datareader["Region"] == DBNull.Value ? "" : (string)datareader["Region"];
+                customers.PostalCode = datareader.GetInt32(6);
+                customers.Country = datareader.GetString(7);
+                customers.Phone = datareader.GetString(8);
+                customers.Fax = datareader.GetString(9);
+
+                customer.Add(customers);
+            }
+
+            MessageBox.Show("Open connection");
+            conexion.Close();
+
+        }
+            
+        
+    }
+}
